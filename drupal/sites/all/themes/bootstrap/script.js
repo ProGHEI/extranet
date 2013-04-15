5fc5bf31a289c4a8d96e2bf6edc338d78f80cab5
Alexandre <Alexandre@Alexandre-PC>
2013-04-15 18:39:54 +0200

    application du thème

---
+++ drupal/sites/all/themes/bootstrap/script.js
@@ -0,0 +1,17 @@
+//$(document).ready(function (){
+//  // listen for scroll
+//    var positionElementInPage = $('#navbar').offset().top;
+//    $(window).scroll(
+//        function() {
+//            if ($(window).scrollTop() >= positionElementInPage) {
+//                // fixed
+//                $('#navbar').addClass("floatable");
+//            } else {
+//                // relative
+//                $('#navbar').removeClass("floatable");
+//            }
+//        }
+//    );
+//    
+//});
+//
